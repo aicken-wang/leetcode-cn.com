839c22896f9fc4d8784296f017382c67b02c4093
wangshunqing <aickenwang@163.com>
2021-03-21 22:32:18 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by shunqing wang on 2021/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
